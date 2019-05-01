e1882d9f0bb7a8bd77a120b09a6c10c27aa8b2b8
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-01 17:12:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + 1).toString();
+    lis[i].innerHTML =  parseInt(lis[i].innerHTML) + n;
   }
 }
 
